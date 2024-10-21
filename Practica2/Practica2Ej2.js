f70f0b28e4f928576afd9e078207f6d61f7ba812
Ayoubb4 <ayoubmisbahfat.al19@emiliojimeno.edu.es>
2024-10-21 19:53:32 +0200

    Primera subida

---
+++ Practica2/Practica2Ej2.js
@@ -0,0 +1,20 @@
+const enlace = document.getElementById('enlaceOpinion');
+const parrafo = document.getElementById('listaOpiniones');
+
+
+function Ejercicio2(){
+    
+    const boton = document.getElementById('boton');
+    
+    const opinionUsuario = prompt('Opinion de la pastica con cafelito');
+        parrafo.innerHTML += opinionUsuario + "<br>";
+        console.log (opinionUsuario)
+        
+/*     
+        enlace.addEventListener('click', function(event) {
+        const opinionUsuario = prompt('Opinion de la pastica con cafelito');
+        parrafo.innerHTML += opinionUsuario + "<br>";
+        console.log (opinionUsuario)
+    }); 
+*/
+}
